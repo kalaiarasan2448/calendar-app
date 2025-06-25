595dbb580a26645f8bc91525b5532a73e192e108
Abishekponmudi <shekabi827@gmail.com>
2025-06-25 22:15:11 +0530

    `Removed unnecessary HTML and Vite configuration for React plugin`

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
 });
-=======
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-});
->>>>>>> bb882fa (Initial commit: fix main.jsx path)
